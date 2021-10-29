93fc988cdab14117e3d4485befc7cb2d2fa0f48e
Youheyo <80012381+Youheyo@users.noreply.github.com>
2021-10-29 20:31:54 +0800

    Fixed up my stupid mistake

--- commands/gaming.js
+++ commands/gaming.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder, roleMention } = require('@discordjs/builders');
 const role = roleMention('877902526706499614');
-const storage = require('../jsonData/emote.json');
+const gamingJSON = require('../jsonData/emote.json');
 
 // admin 291882674564497408
 // gaming 877902526706499614
@@ -8,9 +8,14 @@ const storage = require('../jsonData/emote.json');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('gaming')
-		.setDescription('Notifies the gamers'),
+		.setDescription('Notifies the gamers')
+		.addStringOption(option => option
+			.setName('msg')
+			.setDescription('Message to be included')
+			.setRequired(false)),
 	async execute(interaction) {
-		//await interaction.reply(storage.gaming[Math.floor(Math.random() * storage.length)]);
-		await interaction.reply(`${role}`);
+		const msg = interaction.options.getString('msg');
+		await interaction.reply(gamingJSON.gaming[Math.floor(Math.random() * gamingJSON.gaming.length)]);
+		await interaction.channel.send(`${role} ${msg}`);
 	},
 };
