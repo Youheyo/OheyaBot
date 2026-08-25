a4c94e622807528ba9866e74b05b9bbe4fac9565
Youheyo <80012381+Youheyo@users.noreply.github.com>
2026-08-25 22:15:52 +0800

    Initial msg listener implementation
    Removed all message listener events in general.py
    Can remove the new message_listener.json in favor of the old one since
    UTF encoding is now set
    
    TODO:
     - msg_listener.py is currently bugged
      - Only 1 item in the list is actually being read
      - If reverting back to old json file, may need to change the some
        lines

--- cogs/msg_listener.py
+++ cogs/msg_listener.py
@@ -16,29 +16,38 @@ class msg_listener(commands.Cog):
 			# print(self.triggers)
 			# print(type(self.triggers))
 
-
 	@commands.Cog.listener()
 	@commands.guild_only()
 	async def on_message(self, ctx):
 		'''Sends a message or do action based on message content'''
 
-		# NOTE:   Ensure bot doesn't respond to itself
+		# NOTE: Ensure bot doesn't respond to itself
 		if ctx.author.bot:
 			return
 
 		for i, word_list in enumerate(self.triggers):
+			print("Checking group:",i,len(word_list['keyword']), "words", word_list)
 			for word in word_list['keyword']:
-				if word in ctx.content.lower():
-					response = word_list['response']
-					print(response)
-					await ctx.channel.send(response)
-
-					emoji_id = word_list['reaction']
-					print(emoji_id)
-					emoji: discord.Emoji = ctx.guild.get_emoji(emoji_id)
-					await ctx.add_reaction(word_list['reaction'])
-
-					break
-
+				print("Sample word")
+				# print(f"Checking if {word} is detected in {ctx.content}")
+				# FIX: For loop does not work. It only reads the first word then nothing else. 
+				#try:
+				# 	response = word_list['response']
+				# 	print(response)
+				# 	await ctx.channel.send(response)
+				#
+				# 	emoji_id = word_list['reaction']
+				# 	emoji = ctx.guild.get_emoji(emoji_id)
+				#
+				# 	# * Check if emoji is not any of the list. Default to emoji_id
+				# 	if not isinstance(emoji, (str, discord.Emoji, discord.Reaction)):
+				# 		emoji = emoji_id
+				# 	print(emoji)
+				# 	await ctx.add_reaction(emoji)
+				#
+				# except Exception as e:
+				# 	await ctx.channel.send("An Error Occurred", delete_after=3)
+				# 	print(f"Error: {e}")
+				
 async def setup(bot):
 	await bot.add_cog(msg_listener(bot))
